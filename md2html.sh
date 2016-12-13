344b1edc920c53d58a88e644ee0008fcea6338e1
Stuart Mashaal <stumash1@gmail.com>
2016-12-13 01:18:11 -0500

    Minor edit to md2html.sh
    Hoping that the reason that stumash/github.io/Algorithms_Notes is
    not rendering Mathjax is that the link was http and not https.  So,
    changed the link to https.  Also changed the name of the function
    pandochtmlmath defined in md2html.sh to pandochtmlmath_local since
    that function was already defined on my machine and I didn't want
    namespace conflicts.

--- md2html.sh
+++ md2html.sh
@@ -2,16 +2,16 @@
 
 ### wrapper function for making html from markdown using custom css files
 # ${1} input filename
-function pandochtmlmath() {
+function pandochtmlmath_local() {
     HTMLFILENAME=$(echo ${1} | sed 's/\.md/.html/')
-    pandoc -s -S --toc --mathjax=http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML --css pandoc.css ${1} -o ${HTMLFILENAME}
+    pandoc -s -S --toc --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML --css pandoc.css ${1} -o ${HTMLFILENAME}
 }
 
 # run pandochtmlmath on every .md in the directory tree
 find $(pwd) -regextype posix-extended -regex '.*\.md$' |
 while read fl; do
     if [ "$fl" != "$(realpath README.md)" ]; then
-        pandochtmlmath $fl
+        pandochtmlmath_local $fl
         HTMLFILENAME=$(echo $fl | sed 's/\.md/.html/')
         CSSPATH=$(realpath --relative-to=$fl pandoc.css)
         sed -i "s#pandoc\.css#$CSSPATH#" $HTMLFILENAME
